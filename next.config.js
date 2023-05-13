7a134b4f2a113dd5e69ac037c5e368c884a72f19
daniiancu <dani.iancu@yahoo.com>
2023-05-13 09:55:27 +0100

    modified auth0 file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['s.gravatar.com','lh3.googleusercontent.com','platform-lookaside.fbsbx.com'],
+    domains: ['s.gravatar.com'],
   }
 }
 
